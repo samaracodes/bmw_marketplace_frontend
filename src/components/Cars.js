15f559db8b37da3b3ba911500f2162e3146c7d6e
Sam <samara.amanda01@gmail.com>
2021-12-29 21:12:13 -0500

    fully setup cars page

---
+++ src/components/Cars.js
@@ -0,0 +1,113 @@
+import react from 'react';
+
+const Cars = () => {
+    return (
+        <div className="cars-page-container">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="cars-page-carousel carousel-item active">
+                        <img src="g82-m4-santorini-blue.jpeg" class="d-block w-100 img-fluid" alt="BMW M Trio Banner" id="carousel-image"/>
+                        <div class="carousel-caption d-none d-md-block">
+                            
+                            
+
+                            <div class="cars-page-search-box input-group input-group-sm col-md-8">
+                                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search by model, year, chassis, color, etc..."/>
+                            </div>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <section class="feature py-0" id="cars-section-border">
+                <div class="cars-section-1 container py-5">
+
+                    <h3>Shop All Cars</h3>
+
+                    <div class="row row-cols-1 row-cols-lg-3 g-6">
+                        <div class="col">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                    <img src="imola-red-m3.jpg" class="card-img" alt="..."/><p></p>
+                                    <h5 class="card-title" id="cars-card-title">2018 BMW F80 M3 - Imola Red</h5>
+                                    <small class="cars-card-info">14,721 miles -- Located in Brooklyn, NY</small><br></br>
+                                    <small class="cars-card-auction-info">ends in 2 days...</small><br></br>
+
+                                    <div class="col">
+                                        <div class="row justify-content-md-center">
+                                            <div class="col-6">
+                                                <h6 id="cars-card-listing-type">Auction</h6>
+                                                <p id="cars-card-listing-price">$25,875.00</p>
+                                                <a href="#" class="btn btn-outline-secondary bid-btn">Make A Bid</a>
+                                            </div>
+                                            <div class="col-6">
+                                                <h6 id="cars-card-listing-type">Buy It Now</h6>
+                                                <p id="cars-card-listing-price">$72,000</p>
+                                                <a href="#" class="btn btn-outline-primary buy-btn">Buy It Now</a>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                    <img src="sapphire-black-m3.jpeg" class="card-img" alt="..."/><p></p>
+                                    <h5 class="card-title" id="cars-card-title">2019 BMW F80 M3 - Sapphire Black</h5>
+                                    <small class="cars-card-info">34,564 miles -- Located in Coral Springs, FL</small>
+
+                                    <div class="col">
+                                        <div class="row justify-content-md-center">
+                                            <div class="col-6">
+                                                <h6 id="cars-card-listing-type">Auction</h6>
+                                                <p id="cars-card-listing-price">$9,126.50</p>
+                                                <a href="#" class="btn btn-outline-secondary bid-btn">Make A Bid</a>
+                                            </div>
+                                            <div class="col-6">
+                                                <h6 id="cars-card-listing-type">Buy It Now</h6>
+                                                <p id="cars-card-listing-price">$73,550.00</p>
+                                                <a href="#" class="btn btn-outline-primary buy-btn">Buy It Now</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                    <img src="champagne-quartz.jpeg" class="card-img" alt="..."/><p></p>
+                                    <h5 class="card-title" id="cars-card-title">2015 BMW F80 M3 - Champagne Quartz</h5>
+                                    <small class="cars-card-info">36,953 miles -- Located in Newark, NJ</small><br></br>
+                                    <small class="cars-card-auction-info">ends on 2/2/22...</small><br></br>
+
+
+                                    <div class="col">
+                                        <div class="row justify-content-md-center">
+                                            <div class="col-7">
+                                                <h6 id="cars-card-listing-type">Raffle</h6>
+                                                <p id="cars-card-listing-price">$300 per ticket</p>
+                                                <a href="#" class="btn btn-outline-primary buy-btn">Enter Giveaway</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Cars
--- src/components/CarsHomepage.js
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-
-const CarsHomepage = () => {
-    return (
-        <div>Cars Home Page</div>
-    )
-}
-
-export default CarsHomepage
