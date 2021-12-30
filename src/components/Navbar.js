267e56157f402d73cbf5d21fd19cea172ed3d981
Sam <samara.amanda01@gmail.com>
2021-12-30 17:17:34 -0500

    replaced brand logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
             <nav class="navbar fixed-top navbar-expand-md navbar-light p-md-3">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/">
-                        <img src="bmw-brand-logo.png" class="navbar-logo" alt="BMW Marketplace Brand Logo" height="120"/>
+                        <img src="bmw-mrkt-logo.png" class="navbar-logo" alt="BMW Marketplace Brand Logo" height="100"/>
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
