b981f73c287be8036d8197b78e7a61c9fce34a6e
Sam <samara.amanda01@gmail.com>
2022-02-09 22:51:27 -0500

    changed footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,35 +6,34 @@ const Footer = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col about-company">
-                            <h4 className="mt-lg-0 mt-sm-3">About Us</h4>
-                            <p className="text-white">BMW Marketplace is a space for enthusiasts to enjoy. </p>
+                            <h4 className="mt-lg-0 mt-sm-3">Follow Us!</h4>
+                            <p className="text-white">See what's going on our social media. <br></br>BMW Mrkt is coming soon on the App Store 😊</p>
+
+                            <p><a href="https://instagram.com/bmwmrkt"> Our Instagram</a></p>
+
                         </div>
 
                         <div className="col links">
                             <h4 className="mt-lg-0 mt-sm-3">See More</h4>
                                 
-                            <p><a href="/signup">Signup </a></p>
+                            <p><a href="/about">About Us</a></p>
 
-                            <p><a href="/cars">Buy Cars</a></p>
-                            
-                            <p><a href="/parts">Buy Parts</a></p>
+                            <p><a href="/selling">Sell Your Stuff</a></p>
 
-                            <p><a href="/na">Sell Your Items</a></p>
-
-                            <p><a href="/na">Service & Repair</a></p>
+                            <p><a href="/service">Service & Repair</a></p>
                         </div>
 
-                        <div className="col location">
-                            <h4 className="mt-lg-0 mt-sm-3">Location</h4>
-                            <p>123 Main Street, New York, NY 10001</p>
-                            <p className="mb-0"><i className="fa fa-phone mr-3"></i>(212) 123-4567</p>
-                            <p><i className="fa fa-envelope-o mr-3"></i>contact@bmwmarketplace.com</p>
+                        <div className="col contact">
+                            <h4 className="mt-lg-0 mt-sm-3">Contact Us</h4>
+                           
+                            <p className="text-white"> If you need to reach out to us, please email us at: <a href="mailto:contact@bmwmrkt.com" className="fa fa-envelope-o mr-3">contact@bmwmrkt.com</a>
+                            </p>
                         </div>
                     </div>
 
                     <div className="row mt-2">
                         <div className="col copyright">
-                        <p className=""><small className="text-white-50">© 2021 BMW Marketplace. All Rights Reserved.</small></p>
+                        <p className=""><small className="text-white-50"> 2022 BMW Marketplace.</small></p>
                         </div>
                     </div>
                 </div>
