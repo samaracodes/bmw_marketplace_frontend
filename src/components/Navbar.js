de20e33cc260bffa2db712ae4fac35e625a07901
Sam <samara.amanda01@gmail.com>
2022-02-09 22:52:24 -0500

    adjusted nav logo, nav links, background color, and dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,45 +4,56 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="navbar-container">
-            <nav className="navbar sticky-top navbar-expand-md navbar-light p-md-3">
+            <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark p-md-0">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
-                        <img src="bmw-mrkt-logo.png" className="navbar-logo" alt="BMW Marketplace Brand Logo" height="100"/>
+                        <img src="bmw-mrkt-logo.png" className="navbar-logo" alt="BMW Marketplace Brand Logo" height="90"/>
                     </a>
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0 al">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/cars">Buy Cars</a>
+                                <a className="nav-link" aria-current="page" href="/">Home</a>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/parts">Buy Parts</a>
+                                <a className="nav-link" aria-current="page" href="/cars">Cars</a>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/selling">Selling</a>
+                                <a className="nav-link" href="/parts">Parts</a>
                             </li>
 
                             <li className="nav-item">
                                 <a className="nav-link" href="/service">Service & Repair</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/service">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                    </svg>
+                            </li> 
+ 
+
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    More
                                 </a>
-                            </li>                        
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="/about">About</a></li>
+                                    <li><a className="dropdown-item" href="/selling">Sell your stuff</a></li>
+                                    <li><a className="dropdown-item" href="#">Become a member</a></li>
+                                </ul>
+                            </li>              
                         </ul>
 
-                        <ul className="navbar-nav mr-auto mb-2 mb-lg-0 al">
+
+                        <ul className="nav navbar-nav navbar-right">
                             <li className="nav-item">
-                                <Link to="/login">Login</Link> | 
-                                <Link to="/signup"> Sign Up</Link>
+                                <a className="nav-link btn signup-login-btn" href="/signup">Signup</a>
+                            </li>
+                            ||                             
+                            <li className="nav-item">
+                                <a className="nav-link signup-login-btn" href="/login">Login</a>
                             </li>
                         </ul>
                     </div>
