196322a966830e0a75110f1592c03a9af1fbedf4
Sam <samara.amanda01@gmail.com>
2022-02-11 20:39:23 -0500

    mapped current user to state, passed current user info to Navbar

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -7,17 +7,28 @@ import About from '../components/About';
 import Login from '../components/Login';
 import Logout from '../components/Logout';
 import Signup from '../components/Signup';
+import Navbar from '../components/Navbar';
+import { getCurrentUser } from '../actions/currentUser'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
 
 
 class MainContainer extends Component {
 
+    componentDidMount() {
+        this.props.getCurrentUser()
+    }
+
     render() {
+        const {loggedIn} = this.props
         return (
             <Router>
+                <Navbar loggedIn={loggedIn} />
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/login" component={Login} />
-                    <Route exact path="/logout" component={Logout} />
                     <Route exact path="/signup" component={Signup} />
                     <Route exact path="/parts" component={Parts} />
                     <Route exact path="/about" component={About} />
@@ -27,4 +38,10 @@ class MainContainer extends Component {
     }
 }
 
-export default MainContainer
+const mapStateToProps = state => {
+    return ({
+      loggedIn: !!state.currentUser
+    })
+}
+
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(MainContainer))
