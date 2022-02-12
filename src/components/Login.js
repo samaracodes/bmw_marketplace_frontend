ee1adfc767eb5e62420ea9b786bfd1403dcc0d5a
Sam <samara.amanda01@gmail.com>
2022-02-11 20:27:45 -0500

    setup login form and logout button, commented out google oauth (will setup another time)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { updateLoginForm } from '../actions/currentUser';
+import { login } from '../actions/currentUser';
 import { GoogleLogin } from 'react-google-login'
 import { refreshTokenSetup } from '../utils/refreshToken'
 
-const clientId = '112746534063-fnqemj6q81k341c94p0gnmmqp4ke4nfc'
+// const clientId = '112746534063-fnqemj6q81k341c94p0gnmmqp4ke4nfc'
 
-const Login = () => {
-    const onSuccess = (res) => {
-        console.log('[Login Success] currentUser:', res.profileObj)
+const Login = ({ loginFormData, updateLoginForm, login, history }) => {
+
+    const formHandler = (e) => {
+        const { name, value } = e.target
+        const updatedFormInfo = {
+            ...loginFormData,
+            [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        login(loginFormData, history)
+    }
+    
+    // const onSuccess = (res) => {
+    //     console.log('[Login Success] currentUser:', res.profileObj)
         
-        //initializing the setup
-        refreshTokenSetup(res)
-    }
+    //     //initializing the setup
+    //     refreshTokenSetup(res)
+    // }
 
-    const onFailure = (res) => {
-        console.log('[Login failed]: res:', res)
-    }
+    // const onFailure = (res) => {
+    //     console.log('[Login failed]: res:', res)
+    // }
+
 
     return (
-        <div>
-            Login Page
-            <GoogleLogin 
+        <div class="signup-form row">
+            <h3 className="display-4">Become a member!</h3>
+            <p>
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <div class="col-md-5 mx-auto mb-4">
+                    <label class="form-label">Email address</label>
+                    <input value={loginFormData.email} name="email" type="text" class="form-control" onChange={formHandler} />
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+
+                <div class="col-md-5 mx-auto mb-4">
+                    <label class="form-label">Password</label>
+                    <input value={loginFormData.password} name="password" type="password" class="form-control" onChange={formHandler}/>
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+    
+            {/* <GoogleLogin 
                 clientId={clientId}
                 buttonText="Login with Google"
                 onSuccess={onSuccess}
@@ -27,11 +65,16 @@ const Login = () => {
                 cookiePolicy={'single_host_origin'}
                 style={{ marginTop: '100px' }}
                 isSignedIn={true}
-            />
-
+            /> */}
         </div>
     )
 } 
 
+const mapStateToProps = state => {
+    return {
+      loginFormData: state.loginForm
+    }
+}
 
-export default Login
+
+export default connect(mapStateToProps, { updateLoginForm, login })(Login)
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,23 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { GoogleLogout } from 'react-google-login'
+import { logout } from '../actions/currentUser'
+import { withRouter } from 'react-router-dom'
 
-const clientId = '112746534063-fnqemj6q81k341c94p0gnmmqp4ke4nfc'
 
-const Logout = () => {
-    const onSuccess = () => {
-        console.log('Logout made successfully ✌🏼')
-    }
+// const clientId = '112746534063-fnqemj6q81k341c94p0gnmmqp4ke4nfc'
+
+const Logout = ({ logout, history }) => {
+    // const onSuccess = () => {
+    //     console.log('Logout made successfully ✌🏼')
+    // }
 
     return (
         <div>
-            <GoogleLogout
+
+            <form onSubmit={(e) => {
+                    e.preventDefault()
+                    logout()
+                    history.push('/')
+                }}>
+                <input type="submit" value="Log Out" className="btn btn-primary" />
+            </form>
+            
+
+            {/* <GoogleLogout
                 clientId={clientId}
                 buttonText="Logout"
                 onLogoutSuccess={onSuccess}
                 onLogout
-            />
+            /> */}
+            
         </div>
     )
 }
 
-export default Logout
+export default withRouter(connect(null, { logout })(Logout))
