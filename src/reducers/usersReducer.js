23d13cdc2ed15972199c24a8b3f19227a1f5c513
Sam <samaracodes@gmail.com>
2023-03-28 23:39:15 -0400

    npm updates, fetch url change from heroku back to localhost

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,9 @@
+//updating the parts of our store that have to do with User
+
+export default function usersReducer(state = {users: []}, action) {
+    switch(action.type) {
+        default:
+            return state
+    }
+
+}
